739f6ca4f4f4c615fc7abf8b9906ed883dfc50b2
Marc Rey <hi@marcrey.ch>
2021-04-05 17:55:47 +0200

    2.2: Stearing and Following the Buggy: Stearing

---
+++ ReysRacingGame/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+    public WheelCollider wheelColliderFL;
+    public WheelCollider wheelColliderFR;
+    public float maxTorque = 500;
+    public float maxSteerAngle = 45;
+
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SetMotorTorque(maxTorque * Input.GetAxis("Vertical"));
+        SetSteerAngle(maxSteerAngle * Input.GetAxis("Horizontal"));
+    }
+
+    void SetSteerAngle(float angle)
+	{
+        wheelColliderFL.steerAngle = angle;
+        wheelColliderFR.steerAngle = angle;
+	}
+
+    void SetMotorTorque(float amount)
+	{
+        wheelColliderFL.motorTorque = amount;
+        wheelColliderFR.motorTorque = amount;
+	}
+}
