2cb66153b67b26ca88be1411ae084064b5d68985
Marc Rey <hi@marcrey.ch>
2021-04-05 18:11:00 +0200

    2.2: Stearing and Following the Buggy: wheel rotation

---
+++ ReysRacingGame/Assets/Scripts/WheelBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelBehaviour : MonoBehaviour
+{
+    public WheelCollider wheelCol;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the wheel position and rotation from the wheelcolider
+		Quaternion quat; 
+        Vector3 position; 
+        wheelCol.GetWorldPose(out position, out quat); 
+        transform.position = position; 
+        transform.rotation = quat;
+    }
+}
