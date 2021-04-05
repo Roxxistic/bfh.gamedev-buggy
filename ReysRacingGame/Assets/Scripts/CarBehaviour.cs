f5d11e90b0ef3534e73fe459e66dce470eeaa2ee
Marc Rey <hi@marcrey.ch>
2021-04-05 18:27:22 +0200

    2.2: Stearing and Following the Buggy: wheel friction

--- ReysRacingGame/Assets/Scripts/CarBehaviour.cs
+++ ReysRacingGame/Assets/Scripts/CarBehaviour.cs
@@ -5,12 +5,18 @@ public class CarBehaviour : MonoBehaviour
 {
     public WheelCollider wheelColliderFL;
     public WheelCollider wheelColliderFR;
+    public WheelCollider wheelColliderBL;
+    public WheelCollider wheelColliderBR;
     public float maxTorque = 500;
     public float maxSteerAngle = 45;
+    public float sidewaysStiffness = 1.5f;
+    public float forewardStiffness = 1.5f;
 
     // Start is called before the first frame update
     void Start()
-    {}
+    {
+        SetWheelFrictionStiffness(forewardStiffness, sidewaysStiffness);
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -30,4 +36,24 @@ public class CarBehaviour : MonoBehaviour
         wheelColliderFL.motorTorque = amount;
         wheelColliderFR.motorTorque = amount;
 	}
+
+    void SetWheelFrictionStiffness(float forewardStiffness, float sidewaysStiffness)
+	{
+        // Leider lässt sich das stiffness-Property nicht direkt setzen, sodass wir über ein WheelFrictionCurve Objekt gehen müssen.
+        WheelFrictionCurve f_fwWFC = wheelColliderFL.forwardFriction; 
+        WheelFrictionCurve f_swWFC = wheelColliderFL.sidewaysFriction;
+        
+        f_fwWFC.stiffness = forewardStiffness; 
+        f_swWFC.stiffness = sidewaysStiffness;
+        
+        wheelColliderFL.forwardFriction = f_fwWFC;
+        wheelColliderFL.sidewaysFriction = f_swWFC;
+        wheelColliderFR.forwardFriction = f_fwWFC;
+        wheelColliderFR.sidewaysFriction = f_swWFC;
+        
+        wheelColliderBL.forwardFriction = f_fwWFC;
+        wheelColliderBL.sidewaysFriction = f_swWFC;
+        wheelColliderBR.forwardFriction = f_fwWFC;
+        wheelColliderBR.sidewaysFriction = f_swWFC;
+    }
 }
