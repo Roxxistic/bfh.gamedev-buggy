92044f13159cfaaed0b22bd9ddbfee24c3608851
Marc Rey <hi@marcrey.ch>
2021-04-11 21:17:08 +0200

    2.1: car frontlights light up the curve.

---
+++ ReysRacingGame/Assets/Scripts/CarFrontlightBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HorizontalDirection
+{
+    Left,
+    Straight,
+    Right
+}
+
+public enum RelativePostion
+{
+    Inner,
+    Outer
+}
+
+public class CarFrontlightBehaviour : MonoBehaviour
+{
+    public WheelCollider wheelCol;
+    public HorizontalDirection direction;
+    public RelativePostion relativePostion;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+		// Get the wheel position and rotation from the wheelcolider
+		Quaternion quat;
+		wheelCol.GetWorldPose(out _, out quat);
+
+        int rotationApprox = (int)(quat.y * 100) % 1000;
+
+        if(
+            ((rotationApprox > 0 && direction == HorizontalDirection.Right) ||
+            (rotationApprox < 0 && direction == HorizontalDirection.Left)) &&
+            relativePostion == RelativePostion.Outer
+            )
+		{
+            Quaternion currentQuat = transform.rotation;
+            Quaternion nextQuat = new Quaternion(currentQuat.x, quat.y / 2, currentQuat.z, currentQuat.w);
+            transform.rotation = nextQuat;
+        }
+    }
+}
