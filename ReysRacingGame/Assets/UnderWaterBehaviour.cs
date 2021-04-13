375b71fdc0e9f4c64dcc231518a3a40877e6cca6
Marc Rey <hi@marcrey.ch>
2021-04-13 23:15:33 +0200

    3: underwater postprocessing.

---
+++ ReysRacingGame/Assets/UnderWaterBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class UnderWaterBehaviour : MonoBehaviour
+{
+    public float waterAltitude;
+
+    private PostProcessLayer layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layer = GetComponent<PostProcessLayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        layer.enabled = transform.position.y < waterAltitude;
+	}
+}
