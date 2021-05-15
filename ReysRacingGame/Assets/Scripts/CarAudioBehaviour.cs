6d80e26298560fc69381bc9090dbd3f439f0af50
Marc Rey <hi@marcrey.ch>
2021-05-15 10:14:14 +0200

    CP5: adds basic audio.

---
+++ ReysRacingGame/Assets/Scripts/CarAudioBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAudioBehaviour : MonoBehaviour
+{
+    public AudioClip engineSingleRPMSoundClip;
+    private AudioSource _engineAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Configure AudioSource component by program
+        _engineAudioSource = gameObject.AddComponent<AudioSource>();
+        _engineAudioSource.clip = engineSingleRPMSoundClip;
+        _engineAudioSource.loop = true;
+        _engineAudioSource.volume = 0.7f;
+        _engineAudioSource.playOnAwake = true;
+        _engineAudioSource.enabled = false; // Bugfix
+        _engineAudioSource.enabled = true; // Bugfix
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
