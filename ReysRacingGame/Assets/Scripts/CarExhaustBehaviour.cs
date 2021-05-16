36583de10076b043bb9e95a12f68f46891af13ca
Marc Rey <hi@marcrey.ch>
2021-05-16 16:23:16 +0200

    CP5: exhaust smoke emission.

---
+++ ReysRacingGame/Assets/Scripts/CarExhaustBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class CarExhaustBehaviour : MonoBehaviour
+	{
+
+		public CarBehaviour engine;
+
+		private ParticleSystem.EmissionModule _smokeEmission;
+		private ParticleSystem _ps;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			_ps = GetComponent<ParticleSystem>();
+			_smokeEmission = _ps.emission;
+			_smokeEmission.enabled = true;
+		}
+
+		// Update is called once per frame
+		void FixedUpdate()
+		{
+			SetSmokeRate(engine.CurrentSpeedRPM);
+		}
+
+		void SetSmokeRate(float engineRPM)
+		{
+			float smokeRate = engineRPM / 50.0f;
+			_smokeEmission.rateOverDistance = new ParticleSystem.MinMaxCurve(smokeRate);
+		}
+	}
+}
