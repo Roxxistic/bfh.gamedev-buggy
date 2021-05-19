b1592283d55e01818a42e046a4f5add16d24b93f
Marc Rey <hi@marcrey.ch>
2021-05-19 15:30:51 +0200

    CP 6: countdown audio.

---
+++ ReysRacingGame/Assets/Scripts/TimingAudioBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingAudioBehaviour : MonoBehaviour
+{
+    public AudioClip countdownSoundClip;
+    public TimingBehaviour timingBehaviourScript;
+
+    private AudioSource _audioSource;
+    private int _previousCountdown = -1;
+    private static float _defaultPitch = 1.0f;
+    private static float _countdownZeroPitch = 2.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Configure AudioSource component by program
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.clip = countdownSoundClip;
+        _audioSource.loop = false;
+        _audioSource.volume = 1.0f;
+        _audioSource.playOnAwake = false;
+        _audioSource.enabled = false; // Bugfix
+        _audioSource.enabled = true; // Bugfix
+
+
+        _previousCountdown = timingBehaviourScript.countMax + 1;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SetCountdownSound(timingBehaviourScript.CountDown);
+    }
+
+    void SetCountdownSound(int countdown)
+    {
+        if (_audioSource == null) return;
+
+        // Only beep once when zero.
+        if (countdown == 0 && _previousCountdown == 0) return;
+
+        _audioSource.pitch = countdown > 0 ? _defaultPitch : _countdownZeroPitch;
+
+        if (_previousCountdown > countdown)
+		{
+            Debug.Log("countdown decreased");
+            _audioSource.Play();
+            _previousCountdown--;
+        }
+    }
+}
