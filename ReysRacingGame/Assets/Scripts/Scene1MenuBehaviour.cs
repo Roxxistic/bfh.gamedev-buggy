4aaca16e88701a9e59d65e2aad1deaff5797a6ca
Marc Rey <hi@marcrey.ch>
2021-06-07 01:47:41 +0200

    CP7: use configuration.

--- ReysRacingGame/Assets/Scripts/Scene1MenuBehaviour.cs
+++ ReysRacingGame/Assets/Scripts/Scene1MenuBehaviour.cs
@@ -3,6 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class Scene1MenuBehaviour : MonoBehaviour
 {
+	private Prefs _prefs;
+
+	public WheelCollider wheelColliderFL;
+	public WheelCollider wheelColliderFR;
+	public WheelCollider wheelColliderRL;
+	public WheelCollider wheelColliderRR;
+
+	public GameObject buggy;
+	public GameObject FlagLeft;
+	public GameObject FlagRight;
+
+	void Start()
+	{
+		_prefs = new Prefs();
+		_prefs.Load();
+		_prefs.SetAll(ref buggy, ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR, ref FlagLeft, ref FlagRight);
+	}
+
 	public void OnMenuClick()
 	{
 		SceneManager.LoadScene("SceneMenu");
