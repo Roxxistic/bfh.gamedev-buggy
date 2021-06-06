d49313e4a5a2038ed81159f603e9ab395b381d9d
Marc Rey <hi@marcrey.ch>
2021-06-07 01:41:34 +0200

    CP7: configuration.

--- ReysRacingGame/Assets/Scripts/MenuBehaviour.cs
+++ ReysRacingGame/Assets/Scripts/MenuBehaviour.cs
@@ -8,8 +8,29 @@ public class MenuBehaviour : MonoBehaviour
 	public WheelCollider wheelColliderFR;
 	public WheelCollider wheelColliderRL;
 	public WheelCollider wheelColliderRR;
+
+	public GameObject buggy;
+	public GameObject FlagLeft;
+	public GameObject FlagRight;
+
 	public Slider sliderSuspensionDistance;
+	public Slider sliderSuspensionSpring;
+	public Slider sliderSuspensionDamper;
+	public Slider sliderWheelFrictionForeward;
+	public Slider sliderWheelFrictionSideways;
+	public Slider sliderBodyColorHue;
+	public Slider sliderBodyColorSaturation;
+	public Slider sliderBodyColorLuminance;
+	public Toggle toggleBodyFeatureFlag;
+
 	public Text textDistanceNumber;
+	public Text textSuspensionSpringNumber;
+	public Text textSuspensionDamperNumber;
+	public Text textWheelFrictionForewardNumber;
+	public Text textWheelFrictionSidewaysNumber;
+	public Text textBodyColorHueNumber;
+	public Text textBodyColorSaturationNumber;
+	public Text textBodyColorLuminanceNumber;
 
 	private Prefs _prefs;
 
@@ -17,9 +38,23 @@ public class MenuBehaviour : MonoBehaviour
 	{
 		_prefs = new Prefs();
 		_prefs.Load();
-		_prefs.SetAll(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+		_prefs.SetAll(ref buggy, ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR, ref FlagLeft, ref FlagRight);
 		sliderSuspensionDistance.value = _prefs.suspensionDistance;
+		sliderSuspensionSpring.value = _prefs.suspensionSpring;
+		sliderSuspensionDamper.value = _prefs.suspensionDamper;
+		sliderWheelFrictionForeward.value = _prefs.wheelFrictionForeward;
+		sliderWheelFrictionSideways.value = _prefs.wheelFrictionSideways;
+		sliderBodyColorHue.value = _prefs.bodyColorHue;
+		sliderBodyColorSaturation.value = _prefs.bodyColorSaturation;
+		sliderBodyColorLuminance.value = _prefs.bodyColorLuminance;
 		textDistanceNumber.text = sliderSuspensionDistance.value.ToString("0.00");
+		textSuspensionSpringNumber.text = sliderSuspensionSpring.value.ToString("0.00");
+		textSuspensionDamperNumber.text = sliderSuspensionDamper.value.ToString("0.00");
+		textWheelFrictionForewardNumber.text = sliderWheelFrictionForeward.value.ToString("0.00");
+		textWheelFrictionSidewaysNumber.text = sliderWheelFrictionSideways.value.ToString("0.00");
+		textBodyColorHueNumber.text = sliderBodyColorHue.value.ToString("0.00");
+		textBodyColorSaturationNumber.text = sliderBodyColorSaturation.value.ToString("0.00");
+		textBodyColorLuminanceNumber.text = sliderBodyColorLuminance.value.ToString("0.00");
 	}
 
 	public void OnSliderChangedSuspensionDistance(float nextValue)
@@ -27,6 +62,61 @@ public class MenuBehaviour : MonoBehaviour
 		textDistanceNumber.text = sliderSuspensionDistance.value.ToString("0.00");
 		_prefs.suspensionDistance = sliderSuspensionDistance.value;
 		_prefs.SetWheelColliderSuspension(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+	}
+
+	public void OnSliderChangedSuspensionSpring(float nextValue)
+	{
+		textSuspensionSpringNumber.text = sliderSuspensionSpring.value.ToString("0.00");
+		_prefs.suspensionSpring = sliderSuspensionSpring.value;
+		_prefs.SetWheelColliderSuspension(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+	}
+
+	public void OnSliderChangedSuspensionDamper(float nextValue)
+	{
+		textSuspensionDamperNumber.text = sliderSuspensionDamper.value.ToString("0.00");
+		_prefs.suspensionDamper = sliderSuspensionDamper.value;
+		_prefs.SetWheelColliderSuspension(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+	}
+
+	public void OnSliderChangedWheelFrictionForeward(float nextValue)
+	{
+		textWheelFrictionForewardNumber.text = sliderWheelFrictionForeward.value.ToString("0.00");
+		_prefs.wheelFrictionForeward = sliderWheelFrictionForeward.value;
+		_prefs.SetWheelColliderSuspension(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+	}
+
+	public void OnSliderChangedWheelFrictionSideways(float nextValue)
+	{
+		textWheelFrictionSidewaysNumber.text = sliderWheelFrictionSideways.value.ToString("0.00");
+		_prefs.wheelFrictionSideways = sliderWheelFrictionSideways.value;
+		_prefs.SetWheelColliderSuspension(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+	}
+
+	public void OnSliderChangedBodyColorHue(float nextValue)
+	{
+		textBodyColorHueNumber.text = sliderBodyColorHue.value.ToString("0.00");
+		_prefs.bodyColorHue = sliderBodyColorHue.value;
+		_prefs.SetBodyColor(ref buggy);
+	}
+
+	public void OnSliderChangedBodyColorSaturation(float nextValue)
+	{
+		textBodyColorSaturationNumber.text = sliderBodyColorSaturation.value.ToString("0.00");
+		_prefs.bodyColorSaturation = sliderBodyColorSaturation.value;
+		_prefs.SetBodyColor(ref buggy);
+	}
+
+	public void OnSliderChangedBodyColorLuminance(float nextValue)
+	{
+		textBodyColorLuminanceNumber.text = sliderBodyColorLuminance.value.ToString("0.00");
+		_prefs.bodyColorLuminance = sliderBodyColorLuminance.value;
+		_prefs.SetBodyColor(ref buggy);
+	}
+
+	public void OnToggleBodyFeatureFlag(bool nextValue)
+	{
+		_prefs.bodyFeatureFlags = toggleBodyFeatureFlag.isOn;
+		_prefs.SetFeatureFlag(ref FlagLeft, ref FlagRight);
 	}
 
 	public void OnStartClick()
--- ReysRacingGame/Assets/Scripts/Prefs.cs
+++ ReysRacingGame/Assets/Scripts/Prefs.cs
@@ -2,21 +2,67 @@
 
 public class Prefs
 {
+	public readonly string suspensionDistanceKey = "suspensionDistance";
+	public readonly string suspensionSpringKey = "suspensionSpring";
+	public readonly string suspensionDamperKey = "suspensionDamper";
+	public readonly string wheelFrictionForewardKey = "wheelFrictionForeward";
+	public readonly string wheelFrictionSidewaysKey = "wheelFrictionSideways";
+	public readonly string bodyColorHueKey = "bodyColorHue";
+	public readonly string bodyColorSaturationKey = "bodyColorSaturation";
+	public readonly string bodyColorLuminanceKey = "bodyColorLuminance";
+	public readonly string bodyFeatureFlagsKey = "bodyFeatureFlags";
+
+	public readonly float suspensionDistanceDefault = 0.2f;
+	public readonly float suspensionSpringDefault = 33674f;
+	public readonly float suspensionDamperDefault = 5306f;
+	public readonly float wheelFrictionForewardDefault = 5.0f;
+	public readonly float wheelFrictionSidewaysDefault = 2.1f;
+	public readonly float bodyColorHueDefault = 1.0f;
+	public readonly float bodyColorSaturationDefault = 1.0f;
+	public readonly float bodyColorLuminanceDefault = 1.0f;
+	public readonly bool bodyFeatureFlagsDefault = true;
+
 	public float suspensionDistance;
+	public float suspensionSpring;
+	public float suspensionDamper;
+	public float wheelFrictionForeward;
+	public float wheelFrictionSideways;
+	public float bodyColorHue;
+	public float bodyColorSaturation;
+	public float bodyColorLuminance;
+	public bool bodyFeatureFlags;
 
 	public void Load()
 	{
-		suspensionDistance = PlayerPrefs.GetFloat("suspensionDistance", 0.2f);
+		suspensionDistance = PlayerPrefs.GetFloat(suspensionDistanceKey, suspensionDistanceDefault);
+		suspensionSpring = PlayerPrefs.GetFloat(suspensionSpringKey, suspensionSpringDefault);
+		suspensionDamper = PlayerPrefs.GetFloat(suspensionDamperKey, suspensionDamperDefault);
+		wheelFrictionForeward = PlayerPrefs.GetFloat(wheelFrictionForewardKey, wheelFrictionForewardDefault);
+		wheelFrictionSideways = PlayerPrefs.GetFloat(wheelFrictionSidewaysKey, wheelFrictionSidewaysDefault);
+		bodyColorHue = PlayerPrefs.GetFloat(bodyColorHueKey, bodyColorHueDefault);
+		bodyColorSaturation = PlayerPrefs.GetFloat(bodyColorSaturationKey, bodyColorSaturationDefault);
+		bodyColorLuminance = PlayerPrefs.GetFloat(bodyColorLuminanceKey, bodyColorLuminanceDefault);
+		bodyFeatureFlags = PlayerPrefs.GetInt(bodyFeatureFlagsKey) == 1;
 	}
 
 	public void Save()
 	{
-		PlayerPrefs.SetFloat("suspensionDistance", suspensionDistance);
+		PlayerPrefs.SetFloat(suspensionDistanceKey, suspensionDistance);
+		PlayerPrefs.SetFloat(suspensionSpringKey, suspensionSpring);
+		PlayerPrefs.SetFloat(suspensionDamperKey, suspensionDamper);
+		PlayerPrefs.SetFloat(wheelFrictionForewardKey, wheelFrictionForeward);
+		PlayerPrefs.SetFloat(wheelFrictionSidewaysKey, wheelFrictionSideways);
+		PlayerPrefs.SetFloat(bodyColorHueKey, bodyColorHue);
+		PlayerPrefs.SetFloat(bodyColorSaturationKey, bodyColorSaturation);
+		PlayerPrefs.SetFloat(bodyColorLuminanceKey, bodyColorLuminance);
+		PlayerPrefs.SetInt(bodyFeatureFlagsKey, bodyFeatureFlags ? 1 : 0);
 	}
 
-	public void SetAll(ref WheelCollider wheelFL, ref WheelCollider wheelFR, ref WheelCollider wheelRL, ref WheelCollider wheelRR)
+	public void SetAll(ref GameObject buggy, ref WheelCollider wheelFL, ref WheelCollider wheelFR, ref WheelCollider wheelRL, ref WheelCollider wheelRR, ref GameObject flagLeft, ref GameObject flagRight)
 	{
 		SetWheelColliderSuspension(ref wheelFL, ref wheelFR, ref wheelRL, ref wheelRR);
+		SetBodyColor(ref buggy);
+		SetFeatureFlag(ref flagLeft, ref flagRight);
 	}
 
 	public void SetWheelColliderSuspension(ref WheelCollider wheelFL, ref WheelCollider wheelFR, ref WheelCollider wheelRL, ref WheelCollider wheelRR)
@@ -25,5 +71,44 @@ public class Prefs
 		wheelFR.suspensionDistance = suspensionDistance;
 		wheelRL.suspensionDistance = suspensionDistance;
 		wheelRR.suspensionDistance = suspensionDistance;
+
+		var joint = new JointSpring()
+		{
+			spring = suspensionSpring,
+			damper = suspensionDamper
+		};
+
+		wheelFL.suspensionSpring = joint;
+		wheelFR.suspensionSpring = joint;
+		wheelRL.suspensionSpring = joint;
+		wheelRR.suspensionSpring = joint;
+
+		WheelFrictionCurve f_fwWFC = wheelFL.forwardFriction;
+		WheelFrictionCurve f_swWFC = wheelFL.sidewaysFriction;
+
+		f_fwWFC.stiffness = wheelFrictionForeward;
+		f_swWFC.stiffness = wheelFrictionSideways;
+
+		wheelFL.forwardFriction = f_fwWFC;
+		wheelFL.sidewaysFriction = f_swWFC;
+		wheelFR.forwardFriction = f_fwWFC;
+		wheelFR.sidewaysFriction = f_swWFC;
+
+		wheelRL.forwardFriction = f_fwWFC;
+		wheelRL.sidewaysFriction = f_swWFC;
+		wheelRR.forwardFriction = f_fwWFC;
+		wheelRR.sidewaysFriction = f_swWFC;
+	}
+
+	public void SetBodyColor(ref GameObject buggy) {
+		var renderer = buggy.GetComponent<Renderer>();
+		var color = Color.HSVToRGB(bodyColorHue, bodyColorSaturation, bodyColorLuminance);
+		renderer.material.color = color;
+	}
+
+	public void SetFeatureFlag(ref GameObject flagLeft, ref GameObject flagRight)
+	{
+		flagLeft.SetActive(bodyFeatureFlags);
+		flagRight.SetActive(bodyFeatureFlags);
 	}
 }
